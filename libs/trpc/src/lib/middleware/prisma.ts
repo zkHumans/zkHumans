a1e08026166fe60a3918f9722b0755db8be46fac
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-05-09 18:05:28 -0700

    feat(libs/trpc): add prisma middleware

---
+++ libs/trpc/src/lib/middleware/prisma.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+import { t } from '../server';
+
+/**
+ * Instantiate a single instance PrismaClient and save it on the global object.
+ * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+ */
+const prismaGlobal = global as typeof global & {
+  prisma?: PrismaClient;
+};
+
+export const prisma: PrismaClient =
+  prismaGlobal.prisma ||
+  new PrismaClient({
+    errorFormat: 'minimal',
+    log:
+      process.env['NODE_ENV'] === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env['NODE_ENV'] !== 'production') {
+  prismaGlobal.prisma = prisma;
+}
+
+export const prismaMiddleware = t.middleware(({ next, ctx }) => {
+  return next({
+    ctx: {
+      ...ctx,
+      prisma,
+    },
+  });
+});
