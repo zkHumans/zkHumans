e36b8a0bd698f496d3bc2f3da2083637a5fee272
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-05-11 17:03:18 -0700

    feat(bin): add zkapp-nx.sh
    assist zkapp contracts to work within nx project

---
+++ bin/zkapp-nx.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# assist zkapp contracts to work within nx project with libs
+
+test ! -f ../../nx.json \
+  && echo "ERROR: ${0}: run from within zkapp contracts directory" \
+  && exit 1
+
+ln -sf ../../node_modules . \
+  && mkdir -p node_modules/@zkhumans \
+  && ln -sf \
+    ../../dist/libs/snarky-bioauth \
+    ./node_modules/@zkhumans/
