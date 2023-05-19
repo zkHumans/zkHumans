d502d8b7ae757a2f5ece1b0ff68bea0b87f44cfc
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-05-19 08:05:01 -0700

    refactor(zkapp-zkhumans): remix v2 prep

--- apps/zkapp-zkhumans/app/root.tsx
+++ apps/zkapp-zkhumans/app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import type { LinksFunction, V2_MetaFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -14,11 +14,11 @@ import { UI } from './components';
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'zkHumans',
-  viewport: 'width=device-width,initial-scale=1',
-});
+export const meta: V2_MetaFunction = () => [
+  { charset: 'utf-8' },
+  { title: 'zkHumans' },
+  { viewport: 'width=device-width,initial-scale=1' },
+];
 
 export type AppContextType = {
   cnsl: {
--- apps/zkapp-zkhumans/remix.config.js
+++ apps/zkapp-zkhumans/remix.config.js
@@ -3,7 +3,9 @@
  */
 module.exports = {
   future: {
-    unstable_tailwind: true,
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
   ignoredRouteFiles: ['**/.*'],
@@ -11,6 +13,8 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  serverModuleFormat: 'cjs', // TODO esm
+  tailwind: true,
   watchPaths: ['../../libs'],
 
   // https://remix.run/docs/en/1.15.0/pages/v2
