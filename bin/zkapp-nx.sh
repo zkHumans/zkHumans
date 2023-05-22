476695fa98d600ca89e845c0b5dd6e2a0f88a03c
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-05-22 14:18:00 -0700

    build(bin/zkapp-nx): symlink build directory locally

--- bin/zkapp-nx.sh
+++ bin/zkapp-nx.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-# assist zkapp contracts to work within nx project with libs
+# assist zkapp contracts to work within nx project with nx libs
+# and "zk deploy" compatibility
 
 test ! -f ../../nx.json \
   && echo "ERROR: ${0}: run from within zkapp contracts directory" \
   && exit 1
 
+# "zk deploy" expects compiled output in ./build
+build_dir=$(basename $(pwd))
+ln -sf ../../dist/libs/${build_dir} ./build
+
+# "zk deploy" expects ./node_modules exists
+# - custom tsconfig paths are found there
+# - built nx libraries are manually linked
 ln -sf ../../node_modules . \
   && mkdir -p node_modules/@zkhumans \
   && ln -sf \
