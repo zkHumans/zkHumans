dcbfeacb6c264fffe3d1f961595efa36bcf88cae
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-05-24 10:42:05 -0700

    feat(libs/trpc): health.check: ensure database connection

--- libs/trpc/src/lib/routers/health.ts
+++ libs/trpc/src/lib/routers/health.ts
@@ -1,5 +1,10 @@
+import { prisma } from '../prisma';
 import { t } from '../server';
 
 export const healthRouter = t.router({
-  check: t.procedure.query(() => '1'),
+  check: t.procedure.query(async () => {
+    // ensure a database connection can be established
+    await prisma.smt.count();
+    return 1;
+  }),
 });
