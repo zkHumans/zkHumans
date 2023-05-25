2a81393a53d60935268e2255156240e45b2747ce
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-05-24 21:38:50 -0700

    test(api-e2e): test api heath, smt db/api create/update/restore

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { trpc } from '@zkhumans/trpc';
 
 describe('GET /', () => {
   it('should return a message', async () => {
@@ -7,4 +8,16 @@ describe('GET /', () => {
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello API' });
   });
+
+  it('has process.env.API_URL', async () => {
+    console.log('API_URL', process.env.API_URL);
+    expect(process.env.API_URL).toBeDefined();
+  });
+
+  it('health.check should return a value', async () => {
+    const r = await trpc.health.check.query();
+    expect(r).toBe(1);
+  });
+
+  // TODO: API return env, check it here
 });
