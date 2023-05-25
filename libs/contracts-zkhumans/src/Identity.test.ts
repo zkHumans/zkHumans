ee2a4aa1b81c33353e6697bb17d065c1009d2af9
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-05-24 21:37:37 -0700

    test(contracts-zkhumans): Identity value<->string<->value

---
+++ libs/contracts-zkhumans/src/Identity.test.ts
@@ -0,0 +1,35 @@
+import { Field, PrivateKey } from 'snarkyjs';
+import { jest } from '@jest/globals';
+import { Identity } from './Identity';
+import { smtStringToValue, smtValueToString } from '@zkhumans/utils';
+
+const verbose = false;
+
+const acct = PrivateKey.random();
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const log = (...args: any[]) => {
+  if (verbose) console.log(args);
+};
+
+describe('Identity', () => {
+  jest.setTimeout(1000 * 100);
+
+  // convert Identity to String and back again
+  it('valueToString <--> stringToValue | Identity', () => {
+    const identity = new Identity({
+      publicKey: acct.toPublicKey(),
+      commitment: Field(0),
+    });
+    log('Identity as Value (original)', JSON.stringify(identity));
+
+    const toStr = smtValueToString(identity, Identity);
+    log('Identity as String', toStr);
+
+    const toVal = smtStringToValue(toStr, Identity);
+    log('Identity as Value', JSON.stringify(toVal));
+
+    expect(identity.commitment).toEqual(toVal.commitment);
+    expect(identity.publicKey).toEqual(toVal.publicKey);
+  });
+});
