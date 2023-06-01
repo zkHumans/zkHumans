e0b8f9a37468fae5f67ee921188fb24418304fff
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-06-01 14:34:13 -0700

    feat(zkapp-zkhumans): add placeholders /identities/$id/authn/(new|($authnId/(delete|edit))

---
+++ apps/zkapp-zkhumans/app/routes/identities.$identityId.authn_.$authnId.delete.tsx
@@ -0,0 +1,19 @@
+// import { useAppContext } from '../root';
+import { LoaderArgs, json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+export const loader = async ({ params }: LoaderArgs) => {
+  return json({ params });
+};
+
+export default function TODO() {
+  const { params } = useLoaderData<typeof loader>();
+  // const appContext = useAppContext();
+
+  return (
+    <div className="my-2 border p-2">
+      <h1>TODO: authn/.../delete</h1>
+      <p>{JSON.stringify(params, null, 2)}</p>
+    </div>
+  );
+}
