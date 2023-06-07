af7e9f09751b150a460c7ccb7af012ef16fa5c2f
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-06-06 21:31:35 -0700

    fix(api-e2e): remove axios
    seems to conflict with trpc-client-calling tests,
    especially when run concurrently

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -1,14 +1,6 @@
-import axios from 'axios';
 import { trpc } from '@zkhumans/trpc-client';
 
 describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
-
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
-  });
-
   it('has process.env.API_URL', async () => {
     console.log('API_URL', process.env.API_URL);
     expect(process.env.API_URL).toBeDefined();
