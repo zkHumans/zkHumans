8179443dfd36c9ebd31ce6d6d803d592814ba54b
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-06-20 22:27:33 -0700

    wip:feat(syncer): fetch contract events

--- apps/syncer/src/main.ts
+++ apps/syncer/src/main.ts
@@ -1,1 +1,42 @@
-console.log('Hello World');
+import { Mina, PublicKey, fetchEvents } from 'snarkyjs';
+import { createTRPCClient, trpcWait } from '@zkhumans/trpc-client';
+import { IdentityManager } from '@zkhumans/contracts';
+
+(async () => {
+  console.log('API_URL:', process.env['API_URL']);
+
+  // wait for API
+  const trpc = createTRPCClient(process.env['API_URL']);
+  const status = await trpcWait(trpc, 5, 3_000);
+  if (!status) process.exit(1);
+
+  // get zkApp info from API
+  const meta = await trpc.meta.query();
+  console.log('meta', meta);
+
+  const Network = Mina.Network({
+    mina: [
+      'https://proxy.berkeley.minaexplorer.com/graphql',
+      'https://api.minascan.io/node/berkeley/v1/graphql',
+    ],
+    archive: [
+      'https://archive.berkeley.minaexplorer.com/',
+      'https://api.minascan.io/archive/berkeley/v1/graphql/',
+    ],
+  });
+  Mina.setActiveInstance(Network);
+
+  const events = await fetchEvents({ publicKey: meta.address.IdentityManager });
+  console.log('events', events);
+
+  const zkAppAddress = PublicKey.fromBase58(meta.address.IdentityManager);
+  const zkApp = new IdentityManager(zkAppAddress);
+  console.log('IdentityManager @ ', zkApp.address);
+
+  const networkState = Mina.getNetworkState();
+  console.log('networkState', networkState);
+
+  // X: // Enable graceful stop
+  // X: process.once('SIGINT', () => procMaster.stop());
+  // X: process.once('SIGTERM', () => procMaster.stop());
+})();
