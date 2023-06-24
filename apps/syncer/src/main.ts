193e946504f443c1dad1461f7c447739423fea81
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-06-24 16:48:47 -0700

    wip(sync): loop

--- apps/syncer/src/main.ts
+++ apps/syncer/src/main.ts
@@ -1,8 +1,11 @@
-import { Mina, PublicKey, fetchEvents } from 'snarkyjs';
+import { Mina, PublicKey } from 'snarkyjs';
 import { createTRPCClient, trpcWait } from '@zkhumans/trpc-client';
 import { IdentityManager } from '@zkhumans/contracts';
+import { delay } from '@zkhumans/utils';
 
 console.log('API_URL:', process.env['API_URL']);
+
+const SYNCER_CYCLE_TIME = +(process.env['SYNCER_CYCLE_TIME'] ?? 30_000);
 
 // wait for API
 const trpc = createTRPCClient(process.env['API_URL']);
@@ -13,6 +16,7 @@ if (!status) process.exit(1);
 const meta = await trpc.meta.query();
 console.log('meta', meta);
 
+// configure Mina GraphQL endpoints
 const Network = Mina.Network({
   mina: [
     'https://proxy.berkeley.minaexplorer.com/graphql',
@@ -25,12 +29,44 @@ const Network = Mina.Network({
 });
 Mina.setActiveInstance(Network);
 
-const events = await fetchEvents({ publicKey: meta.address.IdentityManager });
-console.log('events', events);
-
+// init zkApp
 const zkAppAddress = PublicKey.fromBase58(meta.address.IdentityManager);
 const zkApp = new IdentityManager(zkAppAddress);
 console.log('IdentityManager @', meta.address.IdentityManager);
+
+const syncEvents = async () => {
+  console.log('Syncing events...');
+  const events = await zkApp.fetchEvents();
+  console.log(
+    `events on ${zkApp.address.toBase58()}`,
+    events.map((e) => ({ type: e.type, data: JSON.stringify(e.event) }))
+  );
+};
+
+// todo: what is zkApp.account.zkappUri ?
+
+const loop = async () => {
+  try {
+    // ensure API (service + database) availability
+    const r = await trpc.health.check.query();
+    if (r !== 1) throw new Error('API unavailable');
+
+    // restart if zkApp address changes (re-deployed), according to API
+    const { address } = await trpc.meta.query();
+    if (meta.address.IdentityManager !== address.IdentityManager)
+      throw new Error('IdentityManager address changed');
+
+    await syncEvents();
+
+    await delay(SYNCER_CYCLE_TIME);
+  } catch (e) {
+    console.log('Exiting to restart:', e);
+    process.exit(1);
+  }
+  await loop();
+};
+
+await loop();
 
 // ?: const networkState = Mina.getNetworkState();
 // ?: console.log('networkState', networkState);
