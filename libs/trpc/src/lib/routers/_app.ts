e8527c2145d94f340cbaee42255795e17d149dc5
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-06-28 14:43:59 -0700

    feat(libs/trpc): add router store

--- libs/trpc/src/lib/routers/_app.ts
+++ libs/trpc/src/lib/routers/_app.ts
@@ -2,11 +2,13 @@ import { t } from '../server';
 import { healthRouter } from './health';
 import { metaProcedure } from './meta';
 import { smtRouter } from './smt';
+import { storeRouter } from './store';
 
 export const appRouter = t.router({
   health: healthRouter,
   meta: metaProcedure,
   smt: smtRouter,
+  store: storeRouter,
 });
 
 export type AppRouter = typeof appRouter;
