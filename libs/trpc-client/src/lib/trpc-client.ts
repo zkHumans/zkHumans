ae34cc04a7cca7a1f0f8bc8d9c6370621a87bb00
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-06-28 14:47:42 -0700

    wip(libs/trpc-client): default API url to '/api' if process.env['API_URL'] not available
    TODO: confirm working, refactor consumers to simply import trpc

--- libs/trpc-client/src/lib/trpc-client.ts
+++ libs/trpc-client/src/lib/trpc-client.ts
@@ -5,13 +5,25 @@ import type { AppRouter } from '@zkhumans/trpc';
 
 // Note: With the exception of NODE_ENV, process.env variables are not
 // available on statically build client, only server-side where there is an
-// actually running process[1]. The default export uses '/api' as the url. Use
+// actual running process[1]. The default export uses '/api' as the url. Use
 // createTRPCClient to init trpc client with a different api url, such as
 // process.env('API_URL').
 //
 // [1] https://github.com/remix-run/remix/discussions/2928
 
-export const createTRPCClient = (url = '/api') =>
+const getURL = () => {
+  const urlDefault = '/api';
+  try {
+    return process.env['API_URL'] ?? urlDefault;
+  } catch (e) {
+    return urlDefault;
+  }
+};
+
+// TODO: check if this solves "everything"? to just import trpc from here
+
+// ?: export const createTRPCClient = (url = '/api') =>
+export const createTRPCClient = (url = getURL()) =>
   createTRPCProxyClient<AppRouter>({
     transformer: SuperJSON,
     links: [
