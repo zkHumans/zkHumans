d0ca0a70faf6a2c84b659fcab39b2b397025d6a8
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-06-28 14:43:59 -0700

    feat(libs/trpc): add routers store zkapp

--- libs/trpc/src/lib/routers/_app.ts
+++ libs/trpc/src/lib/routers/_app.ts
@@ -2,11 +2,15 @@ import { t } from '../server';
 import { healthRouter } from './health';
 import { metaProcedure } from './meta';
 import { smtRouter } from './smt';
+import { storeRouter } from './store';
+import { zkappRouter } from './zkapp';
 
 export const appRouter = t.router({
   health: healthRouter,
   meta: metaProcedure,
   smt: smtRouter,
+  store: storeRouter,
+  zkapp: zkappRouter,
 });
 
 export type AppRouter = typeof appRouter;
