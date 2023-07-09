ac351fc6918bfafb1da40307124ec1045254df51
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-09 16:49:32 -0700

    refactor(api-e2e): store.{id -> identifier}

--- apps/api-e2e/src/api/store.spec.ts
+++ apps/api-e2e/src/api/store.spec.ts
@@ -5,7 +5,7 @@ import { trpc } from '@zkhumans/trpc-client';
 describe('Store', () => {
   jest.setTimeout(1000 * 100);
 
-  const id = '__TEST__'; // store identifier
+  const identifier = '__TEST__'; // store identifier
   const address = '__TEST__'; // zkapp address
 
   const k1 = Field(100);
@@ -18,8 +18,8 @@ describe('Store', () => {
     // Get / Clear (for fresh test)
     ////////////////////////////////////
 
-    if (await trpc.store.byId.query({ id }))
-      await trpc.store.delete.mutate({ id });
+    if (await trpc.store.byId.query({ identifier }))
+      await trpc.store.delete.mutate({ identifier });
 
     if (await trpc.zkapp.byAddress.query({ address }))
       await trpc.zkapp.delete.mutate({ address });
@@ -36,7 +36,7 @@ describe('Store', () => {
 
     // create store in database
     let dbMM = await trpc.store.create.mutate({
-      id,
+      identifier,
       commitment: '',
       zkapp: { address },
     });
@@ -48,7 +48,7 @@ describe('Store', () => {
     // add to the MM
     mm1.set(k1, v1);
     await trpc.store.set.mutate({
-      store: { id },
+      store: { identifier },
       key: k1.toString(),
       value: v1.toString(),
     });
@@ -56,7 +56,7 @@ describe('Store', () => {
     // add to the MM
     mm1.set(k2, v2);
     await trpc.store.set.mutate({
-      store: { id },
+      store: { identifier },
       key: k2.toString(),
       value: v2.toString(),
     });
@@ -64,12 +64,12 @@ describe('Store', () => {
     // delete from MM (set to "empty")
     mm1.set(k1, Field(0));
     await trpc.store.set.mutate({
-      store: { id },
+      store: { identifier },
       key: k1.toString(),
       value: Field(0).toString(),
     });
 
-    dbMM = await trpc.store.byId.query({ id });
+    dbMM = await trpc.store.byId.query({ identifier });
     console.log('dbMM', dbMM);
 
     ////////////////////////////////////
@@ -80,9 +80,9 @@ describe('Store', () => {
 
     // get store from database, create if not exists
     const dbMM2 =
-      (await trpc.store.byId.query({ id })) ??
+      (await trpc.store.byId.query({ identifier })) ??
       (await trpc.store.create.mutate({
-        id,
+        identifier,
         commitment: '',
         zkapp: { address },
       }));
@@ -110,7 +110,7 @@ describe('Store', () => {
     // Clear
     ////////////////////////////////////
 
-    const s = await trpc.store.delete.mutate({ id });
+    const s = await trpc.store.delete.mutate({ identifier });
     expect(s).toBeTruthy();
 
     const z = await trpc.zkapp.delete.mutate({ address });
