4a504076a5b112fd77a4f6cb6f43c018caf5c432
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-09 15:38:25 -0700

    feat(contracts): add developing IdentityManager methods for (add|del|set)AuthNFactor

--- libs/contracts/src/IdentityManager.ts
+++ libs/contracts/src/IdentityManager.ts
@@ -14,6 +14,9 @@ import {
 import {
   EMPTY,
   EventStore,
+  EventStoreCommit,
+  EventStorePending,
+  RollupTransformationsProof,
   UnitOfStore,
   ZKKV,
   eventStoreDefault,
@@ -132,7 +135,15 @@ export class AuthNFactor extends Struct({
   }
 
   toUnitOfStore(): UnitOfStore {
-    return UnitOfStore.init({ key: this.getKey(), value: this.getValue() });
+    return UnitOfStore.init({
+      key: this.getKey(),
+      value: this.getValue(),
+      meta: [
+        this.protocol.type,
+        this.protocol.provider,
+        this.protocol.revision,
+      ],
+    });
   }
 }
 
@@ -166,22 +177,38 @@ export class Identity extends Struct({
 }
 
 export class IdentityManager extends SmartContract {
-  // Identity Manager Merkle Map; static identifier
+  /**
+   * Static identifier of the Identity Manager Merkle Map.
+   */
   @state(Field) identifier = State<Field>();
 
-  // Identity Manager Merkle Map; root hash
+  /**
+   * Root hash of the Identity Manager Merkle Map that stores committed Identities.
+   */
   @state(Field) commitment = State<Field>();
+
+  /**
+   * A hash used to authenticate storage commitment. Set upon deployment.
+   * This is a developmental alternative to the recursive proof verfication, a
+   * shim to navigate the computational requirements of the recursive proofs.
+   */
+  @state(Field) authHash = State<Field>();
 
   override events = {
     // for updating off-chain data
     'store:new': EventStore,
     'store:set': EventStore,
+    'store:pending': EventStorePending,
+
+    // triggers pending events to be committed
+    'store:commit': EventStoreCommit,
   };
 
   override init() {
     super.init();
     this.identifier.set(EMPTY);
     this.commitment.set(EMPTY);
+    this.authHash.set(EMPTY);
   }
 
   override deploy(args: DeployArgs) {
@@ -199,14 +226,14 @@ export class IdentityManager extends SmartContract {
     const mgrIdentifier = this.identifier.getAndAssertEquals();
     const mgrCommitment = this.commitment.getAndAssertEquals();
 
-    const events = ZKKV.addStore(
-      identity.toUnitOfStore(),
-      UnitOfStore.init({
+    const events = ZKKV.addStore({
+      store: identity.toUnitOfStore(),
+      storeManager: UnitOfStore.init({
         key: mgrIdentifier,
         value: mgrCommitment,
       }),
-      witnessManager
-    );
+      witnessManager,
+    });
 
     for (const { type, event } of events) this.emitEvent(type, event);
 
@@ -220,7 +247,162 @@ export class IdentityManager extends SmartContract {
 
   /**
    * Add an Authentication Factor to an Identity.
+   *
+   * @param {AuthNFactor} authNFactor The AuthNFactor with new value to update.
+   * @param {Identity} identity The Identity (Store) to add the AuthNFactor (data) to.
+   * @param {MerkleMapWitness} witnessKeyring Witness for AuthNFactor (data) within Identity (Store).
+   * @param {MerkleMapWitness} witnessManager Witness for Identity (Store) within Manager.
    */
+  @method NEW_addAuthNFactor(
+    authNFactor: AuthNFactor,
+    identity: Identity,
+    witnessKeyring: MerkleMapWitness,
+    witnessManager: MerkleMapWitness
+  ) {
+    const mgrIdentifier = this.identifier.getAndAssertEquals();
+    const mgrCommitment = this.commitment.getAndAssertEquals();
+
+    const events = ZKKV.setStoreData({
+      data0: UnitOfStore.init({
+        key: authNFactor.toUnitOfStore().getKey(),
+        value: EMPTY, // Add
+      }),
+      data1: authNFactor.toUnitOfStore(),
+      store: identity.toUnitOfStore(),
+      storeManager: UnitOfStore.init({
+        key: mgrIdentifier,
+        value: mgrCommitment,
+      }),
+      witnessStore: witnessKeyring,
+      witnessManager,
+    });
+
+    for (const { type, event } of events) this.emitEvent(type, event);
+  }
+
+  /**
+   * Remove an Authentication Factor from an Identity.
+   *
+   * @param {AuthNFactor} authNFactor The AuthNFactor with current value to remove.
+   * @param {Identity} identity The Identity (Store) to remove the AuthNFactor (data) from.
+   * @param {MerkleMapWitness} witnessKeyring Witness for AuthNFactor (data) within Identity (Store).
+   * @param {MerkleMapWitness} witnessManager Witness for Identity (Store) within Manager.
+   */
+  @method NEW_delAuthNFactor(
+    authNFactor: AuthNFactor,
+    identity: Identity,
+    witnessKeyring: MerkleMapWitness,
+    witnessManager: MerkleMapWitness
+  ) {
+    const mgrIdentifier = this.identifier.getAndAssertEquals();
+    const mgrCommitment = this.commitment.getAndAssertEquals();
+
+    const events = ZKKV.setStoreData({
+      data0: authNFactor.toUnitOfStore(),
+      data1: UnitOfStore.init({
+        key: authNFactor.toUnitOfStore().getKey(),
+        value: EMPTY, // Delete
+      }),
+      store: identity.toUnitOfStore(),
+      storeManager: UnitOfStore.init({
+        key: mgrIdentifier,
+        value: mgrCommitment,
+      }),
+      witnessStore: witnessKeyring,
+      witnessManager,
+    });
+
+    for (const { type, event } of events) this.emitEvent(type, event);
+  }
+
+  /**
+   * Set an Authentication Factor within an Identity.
+   *
+   * @param {AuthNFactor} authNFactor0 The current AuthNFactor.
+   * @param {AuthNFactor} authNFactor1 The new AuthNFactor.
+   * @param {Identity} identity The Identity (Store) to update the AuthNFactor (data) within.
+   * @param {MerkleMapWitness} witnessKeyring Witness for AuthNFactor (data) within Identity (Store).
+   * @param {MerkleMapWitness} witnessManager Witness for Identity (Store) within Manager.
+   */
+  @method NEW_setAuthNFactor(
+    authNFactor0: AuthNFactor,
+    authNFactor1: AuthNFactor,
+    identity: Identity,
+    witnessKeyring: MerkleMapWitness,
+    witnessManager: MerkleMapWitness
+  ) {
+    const mgrIdentifier = this.identifier.getAndAssertEquals();
+    const mgrCommitment = this.commitment.getAndAssertEquals();
+
+    const events = ZKKV.setStoreData({
+      data0: authNFactor0.toUnitOfStore(),
+      data1: authNFactor1.toUnitOfStore(),
+      store: identity.toUnitOfStore(),
+      storeManager: UnitOfStore.init({
+        key: mgrIdentifier,
+        value: mgrCommitment,
+      }),
+      witnessStore: witnessKeyring,
+      witnessManager,
+    });
+
+    for (const { type, event } of events) this.emitEvent(type, event);
+  }
+
+  /**
+   * Commit pending transformations with a recursive proof of state transformation.
+   *
+   * @param {RollupTransformationsProof} proof A recursive rollup proof of transformations
+   */
+  @method commitPendingTransformations(proof: RollupTransformationsProof) {
+    const mgrCommitment = this.commitment.getAndAssertEquals();
+
+    // ensure the proof started from the zkApp's current commitment
+    proof.publicInput.root0.assertEquals(mgrCommitment);
+
+    proof.verify();
+
+    // updat the zkApp's commitment
+    this.commitment.set(proof.publicInput.root1);
+
+    // inform storage to commit pending transformations proven on the initial commitment
+    this.emitEvent('store:commit', {
+      commitmentPending: proof.publicInput.root0,
+      commitmentSettled: proof.publicInput.root1,
+    });
+  }
+
+  /**
+   * Commit pending transformations.
+   * Use an authentication token for authorization.
+   *
+   * @param {Field} authToken Secret authentication token.
+   */
+  @method commitPendingTransformationsWithAuthToken(
+    authToken: Field,
+    commitmentPending: Field,
+    commitmentSettled: Field
+  ) {
+    const authHash = this.authHash.getAndAssertEquals();
+    const mgrCommitment = this.commitment.getAndAssertEquals();
+
+    // authorize the request
+    authHash.assertNotEquals(EMPTY, 'Auth not configured!');
+    authHash.assertEquals(Poseidon.hash([authToken]), 'Auth failed!');
+
+    // verify current commitment
+    commitmentPending.assertEquals(mgrCommitment);
+
+    // updat the zkApp's commitment
+    this.commitment.set(commitmentSettled);
+
+    // inform storage to commit pending transformations proven on the initial commitment
+    this.emitEvent('store:commit', {
+      commitmentPending,
+      commitmentSettled,
+    });
+  }
+
   @method addAuthNFactor(
     authNFactor: AuthNFactor,
     id0: Identity,
