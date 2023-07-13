e40865d82703f51799b4cdef5a7cabb0a0272fdf
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-12 21:30:31 -0700

    feat(zkapp-zkhumans): update api type

--- apps/zkapp-zkhumans/app/hooks/useData.tsx
+++ apps/zkapp-zkhumans/app/hooks/useData.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 
-import type { ApiStoreByIdOutput } from '@zkhumans/trpc-client';
+import type { ApiOutputStoreById } from '@zkhumans/trpc-client';
 import type { AppContextType } from '../root';
 import type { CNSL } from './useConsole';
 
 export function useData(cnsl: CNSL, zk: AppContextType['zk']) {
   const [refreshTrigger, setRefreshTrigger] = useState(0);
   const [identities, setIdentities] = useState(
-    [] as NonNullable<ApiStoreByIdOutput>[]
+    [] as NonNullable<ApiOutputStoreById>[]
   );
 
   // get identities upon account change or refresh trigger
