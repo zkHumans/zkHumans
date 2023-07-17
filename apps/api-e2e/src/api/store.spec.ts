9c6ddc07114c6b74e14632d98654f1837461a08f
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-17 16:25:38 -0700

    feat(api-e2e): store -> storage

--- apps/api-e2e/src/api/store.spec.ts
+++ apps/api-e2e/src/api/store.spec.ts
@@ -1,12 +1,26 @@
 import { jest } from '@jest/globals';
 import { Field, MerkleMap } from 'snarkyjs';
-import { trpc } from '@zkhumans/trpc-client';
+import { ApiInputEventCreate, trpc } from '@zkhumans/trpc-client';
 
 describe('Store', () => {
   jest.setTimeout(1000 * 100);
 
-  const identifier = '__TEST__'; // store identifier
-  const address = '__TEST__'; // zkapp address
+  ////////////////////////////////////
+  // test data
+  ////////////////////////////////////
+
+  const t = '__TEST__';
+  const key = t; // store identifier
+  const address = t; // zkapp address
+
+  const event: ApiInputEventCreate = {
+    id: t,
+    type: t,
+    data: t,
+    transactionInfo: t,
+    blockHeight: BigInt(0),
+    globalSlot: BigInt(0),
+  };
 
   const k1 = Field(100);
   const v1 = Field(200);
@@ -18,11 +32,14 @@ describe('Store', () => {
     // Get / Clear (for fresh test)
     ////////////////////////////////////
 
-    if (await trpc.store.byId.query({ identifier }))
-      await trpc.store.delete.mutate({ identifier });
+    if (await trpc.storage.byKey.query({ key }))
+      await trpc.storage.delete.mutate({ key });
 
     if (await trpc.zkapp.byAddress.query({ address }))
       await trpc.zkapp.delete.mutate({ address });
+
+    if (await trpc.event.byId.query({ id: event.id }))
+      await trpc.event.delete.mutate({ id: event.id });
 
     ////////////////////////////////////
     // Create
@@ -34,10 +51,14 @@ describe('Store', () => {
     // crete zkapp in database
     await trpc.zkapp.create.mutate({ address });
 
+    // create event in database
+    await trpc.event.create.mutate(event);
+
     // create store in database
-    let dbMM = await trpc.store.create.mutate({
-      identifier,
-      commitment: '',
+    let dbMM = await trpc.storage.create.mutate({
+      key,
+      value: '',
+      event: { id: event.id },
       zkapp: { address },
     });
 
@@ -45,31 +66,37 @@ describe('Store', () => {
     // Update
     ////////////////////////////////////
 
+    const data = {
+      event: { id: event.id },
+      storage: { key },
+      zkapp: { address },
+    };
+
     // add to the MM
     mm1.set(k1, v1);
-    await trpc.store.set.mutate({
-      store: { identifier },
+    await trpc.storage.set.mutate({
       key: k1.toString(),
       value: v1.toString(),
+      ...data,
     });
 
     // add to the MM
     mm1.set(k2, v2);
-    await trpc.store.set.mutate({
-      store: { identifier },
+    await trpc.storage.set.mutate({
       key: k2.toString(),
       value: v2.toString(),
+      ...data,
     });
 
     // delete from MM (set to "empty")
     mm1.set(k1, Field(0));
-    await trpc.store.set.mutate({
-      store: { identifier },
+    await trpc.storage.set.mutate({
       key: k1.toString(),
       value: Field(0).toString(),
+      ...data,
     });
 
-    dbMM = await trpc.store.byId.query({ identifier });
+    dbMM = await trpc.storage.byKey.query({ key });
     console.log('dbMM', dbMM);
 
     ////////////////////////////////////
@@ -78,16 +105,10 @@ describe('Store', () => {
 
     const mm2 = new MerkleMap();
 
-    // get store from database, create if not exists
-    const dbMM2 =
-      (await trpc.store.byId.query({ identifier })) ??
-      (await trpc.store.create.mutate({
-        identifier,
-        commitment: '',
-        zkapp: { address },
-      }));
+    // get existing storage from database
+    const dbMM2 = await trpc.storage.byKeyWithData.query({ key });
 
-    // restore Merkle Map from db store
+    // restore Merkle Map from db
     for (const data of dbMM2.data) {
       try {
         mm2.set(Field(data.key), Field(data.value));
@@ -110,10 +131,13 @@ describe('Store', () => {
     // Clear
     ////////////////////////////////////
 
-    const s = await trpc.store.delete.mutate({ identifier });
+    const s = await trpc.storage.delete.mutate({ key });
     expect(s).toBeTruthy();
 
     const z = await trpc.zkapp.delete.mutate({ address });
     expect(z).toBeTruthy();
+
+    const e = await trpc.event.delete.mutate({ id: event.id });
+    expect(e).toBeTruthy();
   });
 });
