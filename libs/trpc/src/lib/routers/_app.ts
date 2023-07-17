fe6d9c3d64951ba2ebd6e81b358ac066868d281f
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-17 16:22:47 -0700

    feat(trpc): add router storage

--- libs/trpc/src/lib/routers/_app.ts
+++ libs/trpc/src/lib/routers/_app.ts
@@ -2,14 +2,14 @@ import { t } from '../server';
 import { eventRouter } from './event';
 import { healthRouter } from './health';
 import { metaProcedure } from './meta';
-import { storeRouter } from './store';
+import { storageRouter } from './storage';
 import { zkappRouter } from './zkapp';
 
 export const appRouter = t.router({
   event: eventRouter,
   health: healthRouter,
   meta: metaProcedure,
-  store: storeRouter,
+  storage: storageRouter,
   zkapp: zkappRouter,
 });
 
---
+++ libs/trpc/src/lib/routers/storage.ts
@@ -0,0 +1,150 @@
+import { Prisma } from '@prisma/client';
+import { z } from 'zod';
+import { prisma } from '../middleware/prisma';
+import { t } from '../server';
+
+export const selectStorage = Prisma.validator<Prisma.storageSelect>()({
+  key: true,
+  value: true,
+  meta: true,
+  isPending: true,
+  commitmentPending: true,
+  commitmentSettled: true,
+  settlementChecksum: true,
+  eventId: true,
+  storageKey: true,
+  zkappAddress: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const storageRouter = t.router({
+  // get storage by key
+  byKey: t.procedure
+    .input(z.object({ key: z.string() }))
+    .query(async ({ input: { key } }) => {
+      return await prisma.storage.findUnique({
+        where: { key },
+        select: selectStorage,
+      });
+    }),
+
+  // get storage by key, with data
+  byKeyWithData: t.procedure
+    .input(z.object({ key: z.string() }))
+    .query(async ({ input: { key } }) => {
+      return await prisma.storage.findUnique({
+        where: { key },
+        select: { ...selectStorage, data: { select: selectStorage } },
+      });
+    }),
+
+  // create a unit of key:value storage
+  // use when storage.key is not known
+  // use set when storage.key exists
+  create: t.procedure
+    .input(
+      z.object({
+        key: z.string(),
+        value: z.string().optional(),
+        meta: z.string().optional(),
+        isPending: z.boolean().optional(),
+        commitmentPending: z.string().optional(),
+        commitmentSettled: z.string().optional(),
+        settlementChecksum: z.string().optional(),
+        event: z.object({
+          id: z.string(),
+        }),
+        zkapp: z.object({
+          address: z.string(),
+        }),
+      })
+    )
+    .mutation(async ({ input: { key, event, zkapp, ...data } }) => {
+      return await prisma.storage.create({
+        data: {
+          key,
+          event: { connect: { id: event.id } },
+          zkapp: { connect: { address: zkapp.address } },
+          ...data,
+        },
+        select: selectStorage,
+      });
+    }),
+
+  delete: t.procedure
+    .input(
+      z.object({
+        key: z.string(),
+      })
+    )
+    .mutation(async ({ input: { key } }) => {
+      return await prisma.storage.delete({ where: { key } });
+    }),
+
+  // get a value with the given key in a Store
+  get: t.procedure
+    .input(
+      z.object({
+        storage: z.object({
+          key: z.string(),
+        }),
+        key: z.string(),
+      })
+    )
+    .query(async ({ input: { storage, key } }) => {
+      // there should be only one, but findUnique doesn't work here
+      return await prisma.storage.findFirst({
+        where: { storageKey: storage.key, key },
+        select: selectStorage,
+      });
+    }),
+
+  pending: t.procedure.query(async () => {
+    return await prisma.storage.findMany({
+      where: { isPending: true },
+      orderBy: [{ createdAt: 'asc' }],
+      select: selectStorage,
+    });
+  }),
+
+  // set or create a unit of key:value storage
+  // requires storage with storage.key to exist
+  set: t.procedure
+    .input(
+      z.object({
+        key: z.string(),
+        value: z.string().optional(),
+        meta: z.string().optional(),
+        isPending: z.boolean().optional(),
+        commitmentPending: z.string().optional(),
+        commitmentSettled: z.string().optional(),
+        settlementChecksum: z.string().optional(),
+        event: z.object({
+          id: z.string(),
+        }),
+        storage: z.object({
+          key: z.string(),
+        }),
+        zkapp: z.object({
+          address: z.string(),
+        }),
+      })
+    )
+    .mutation(async ({ input: { key, event, storage, zkapp, ...data } }) => {
+      return await prisma.storage.upsert({
+        where: { key },
+        update: {
+          ...data,
+        },
+        create: {
+          ...data,
+          key,
+          event: { connect: { id: event.id } },
+          storage: { connect: { key: storage.key } },
+          zkapp: { connect: { address: zkapp.address } },
+        },
+        select: selectStorage,
+      });
+    }),
+});
