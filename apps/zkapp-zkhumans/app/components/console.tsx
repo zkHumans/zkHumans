f9eceac9663ba645e23cd530e7536eda1141fc54
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-22 10:40:50 -0700

    feat(zkapp-zkhumans): console component default to open

--- apps/zkapp-zkhumans/app/components/console.tsx
+++ apps/zkapp-zkhumans/app/components/console.tsx
@@ -1,17 +1,23 @@
 import { CommandLineIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 interface ConsoleProps {
   output: string[]; // output log entries in reverse order
 }
 
 export function Console({ output }: ConsoleProps) {
+  const [collapsed, setCollapsed] = useState(false as boolean);
   return (
     <div className="flex flex-col">
       <div
         tabIndex={0}
         className="collapse-arrow border-base-300 bg-base-200 collapse rounded-none border"
       >
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={!collapsed}
+          onChange={() => setCollapsed(!collapsed)}
+        />
         <div className="collapse-title flex flex-row items-center text-xl font-medium">
           <CommandLineIcon className="mr-2 h-5 w-5" strokeWidth="2" /> Console
         </div>
