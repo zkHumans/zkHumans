f0facaee058651eea5c9a4889f4d5b42ace6d0dc
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-29 10:30:17 -0700

    fix(oracle-auth): use payloadFromBase58 from snarky-bioauth

--- apps/oracle-auth/app/mina.server.tsx
+++ apps/oracle-auth/app/mina.server.tsx
@@ -1,16 +1,16 @@
-import {
-  Encoding,
-  Field,
-  Poseidon,
-  PrivateKey,
-  PublicKey,
-  Signature,
-} from 'snarkyjs';
+import { Encoding, Field, Poseidon, PrivateKey, Signature } from 'snarkyjs';
 import invariant from 'tiny-invariant';
+import { payloadFromBase58 } from '@zkhumans/snarky-bioauth';
 
 import { cache } from './cache.server';
 
 const TTL = Number(process.env.AUTH_BIOAUTH_TTL) ?? 1000 * 60 * 10;
+
+invariant(
+  process.env.AUTH_MINA_PRIVATE_KEY,
+  'AUTH_MINA_PRIVATE_KEY must be set'
+);
+const AUTH_MINA_PRIVATE_KEY = process.env.AUTH_MINA_PRIVATE_KEY;
 
 export async function cacheBioAuth(id: string, data: any) {
   cache.set(id, JSON.stringify(data), TTL);
@@ -20,26 +20,15 @@ export async function getCachedBioAuth(id: string): Promise<any | undefined> {
   return cache.has(id) ? JSON.parse(cache.get(id) as string) : undefined;
 }
 
-// from snarky-bioauth library
-function payloadFromBase58(id: string): Field {
-  const publicKey = PublicKey.fromBase58(id);
-  return publicKey.x;
-}
-
 export async function getSignedBioAuth(_id: string, _bioAuthId: string) {
-  invariant(
-    process.env.AUTH_MINA_PRIVATE_KEY,
-    'AUTH_MINA_PRIVATE_KEY must be set'
-  );
-
   // The private key of our account.
-  const privateKey = PrivateKey.fromBase58(process.env.AUTH_MINA_PRIVATE_KEY);
+  const privateKey = PrivateKey.fromBase58(AUTH_MINA_PRIVATE_KEY);
 
   // Compute the public key associated with our private key
   const publicKey = privateKey.toPublicKey();
 
   // Define a Field with the value of the id
-  const payload = Field(payloadFromBase58(_id));
+  const payload = payloadFromBase58(_id);
 
   // Define a Field with the current timestamp
   const timestamp = Field(Date.now());
