b6bf2c6e4644e614a6a1e48bf87b7fb7b8e4103b
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-30 11:29:44 -0700

    feat(contracts): add ExampleIdentityConsumer.somethingRequiringBioAuth()

--- libs/contracts/src/ExampleIdentityConsumer.ts
+++ libs/contracts/src/ExampleIdentityConsumer.ts
@@ -30,4 +30,22 @@ export class ExampleIdentityConsumer extends SmartContract {
 
     this.emitEvent('authorizedID', identifier);
   }
+
+  @method somethingRequiringBioAuth(assertion: IdentityAssertion) {
+    const idMgrPubKey = this.IDManagerPublicKey.getAndAssertEquals();
+    const identityManager = new IdentityManager(idMgrPubKey);
+
+    // assert Identity ownership
+    identityManager.isIdentityOwner(assertion).assertTrue();
+
+    // assert Authentication Factor is a BioAuth
+    assertion.authNF.isBioAuth().assertTrue();
+
+    // do something after Identity ownership has been proven with BioAuth
+
+    // the Identity's unique identifier is useful
+    const identifier = assertion.identity.identifier;
+
+    this.emitEvent('bioauthorizedID', identifier);
+  }
 }
