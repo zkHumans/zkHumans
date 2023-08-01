24d7443244cd59fb319006e4f880775fa326bbe6
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-31 20:36:26 -0700

    feat(zkapp-zkhumans): remove splash subtitle

--- apps/zkapp-zkhumans/app/routes/_index.tsx
+++ apps/zkapp-zkhumans/app/routes/_index.tsx
@@ -2,10 +2,7 @@ export default function Index() {
   return (
     <div className="my-10 flex flex-col items-center space-y-8">
       <div className="flex flex-col items-center">
-        <h1 className="text-2xl font-bold">zkHumans</h1>
-        <h1 className="text-1xl font-bold">
-          Anonymous CryptoBiometric Memberships
-        </h1>
+        <h1 className="text-4xl font-bold">zkHumans</h1>
       </div>
     </div>
   );
