1425e5cc5bbbf4c77dbf82b700fc2e8f652fa7d4
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-07-31 17:35:23 -0700

    chore(zkapp-zkhumans): configure remix to use postcss+autoprefixer
    required for daisyui on mac+chrome
    https://github.com/saadeghi/daisyui/issues/2193

--- apps/zkapp-zkhumans/remix.config.js
+++ apps/zkapp-zkhumans/remix.config.js
@@ -14,6 +14,7 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  postcss: true,
   serverModuleFormat: 'cjs', // TODO esm
   tailwind: true,
   watchPaths: ['../../libs/*/src'],
